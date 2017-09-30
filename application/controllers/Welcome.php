f43f9dc2fb94cf94bd29e76eea3f814503bdcbc2
Yannick Nguyen <yannicknguyen@hotmail.com>
2017-09-30 14:50:34 -0700

    Cleaning up

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,29 +1,27 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends Application
-{
+class Welcome extends Application {
 
-	function __construct()
-	{
-		parent::__construct();
-	}
+    function __construct() {
+        parent::__construct();
+    }
 
-	/**
-	 * Homepage for our app
-	 */
-	public function index()
-	{
-		// this is the view we want shown
-		$this->data['pagebody'] = 'homepage';
+    /**
+     * Homepage for our app
+     */
+    public function index() {
+        // this is the view we want shown
+        $this->data['pagebody'] = 'homepage';
 
-		// build the list of authors, to pass on to our view
-		$source = $this->quotes->all();
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->all();
 
-		// pass on the data to present, as the "authors" view parameter
-		$this->data['authors'] = $source;
+        // pass on the data to present, as the "authors" view parameter
+        $this->data['authors'] = $source;
 
-		$this->render();
-	}
+        $this->render();
+    }
 
 }
