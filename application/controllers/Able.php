9e0c1dddfc05aa55673c844593b6167448f0f48f
Yannick <Yannick@142.232.149.60>
2017-09-30 14:36:47 -0700

    closes #10

--- application/controllers/Able.php
+++ application/controllers/Able.php
@@ -1,5 +1,6 @@
 <?php
 
+defined('BASEPATH') OR exit('No direct script access allowed');
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,7 +12,7 @@
  *
  * @author Yannick
  */
-class Able extends Application{
+class Able extends Application {
 
     public function index() {
         $this->show(1);
